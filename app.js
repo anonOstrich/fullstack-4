0600e78f911fef5c8ffa64fabea1a90061ae8d5a
anonOstrich <jesper.kuutti@helsinki.fi>
2019-01-30 11:35:05 +0200

    Jaettu sovellus eri moduuleihin
    Noudatettu materiaalin esimerkkiä, mutta ei vielä lisätty esimerkiksi middlewareja koska sellaisia ei alkuperäisessä viestissä ollut määritelty.
    Tehty ->4.2]

---
+++ app.js
@@ -0,0 +1,13 @@
+const config = require('./utils/config')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+
+app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
