6a27ce6398e3cde055b5e1655873eff8a52fd4f8
anonOstrich <jesper.kuutti@helsinki.fi>
2019-02-02 18:07:02 +0200

    Tehty ->4.18

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
@@ -12,6 +13,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 app.use(bodyParser.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
